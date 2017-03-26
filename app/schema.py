3ee0585334bbf5a93d7e44bfb84ccabe1708b7cc
Federico Marcos <marcosfede@gmail.com>
2017-03-26 18:54:58 -0300

    mutations

--- app/schema.py
+++ app/schema.py
@@ -1,17 +1,20 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField
-from app.users.schema import User, Role
+from app.users.schema import Query as UsersQuery
+from app.users.schema import Mutation as UsersMutations
 
 
-class Query(graphene.ObjectType):
-    node = relay.Node.Field()
-    all_users = SQLAlchemyConnectionField(User)
-    all_roles = SQLAlchemyConnectionField(Role)
-    role = graphene.Field(Role)
+class Query(UsersQuery, graphene.ObjectType):
+    pass
 
 
-schema = graphene.Schema(query=Query, types=[User, Role])
+class Mutation(UsersMutations, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
 
 default_query = '''
 {
--- app/users/schema.py
+++ app/users/schema.py
@@ -1,5 +1,5 @@
 
-from graphene import relay
+from graphene import relay, AbstractType
 from graphene_sqlalchemy import SQLAlchemyObjectType
 from app.users.models import User as UserModel
 from app.users.models import Role as RoleModel
@@ -18,3 +18,22 @@ class Role(SQLAlchemyObjectType):
     class Meta:
         model = RoleModel
         interfaces = (relay.Node, )
+
+
+class Query(AbstractType):
+    node = relay.Node.Field(User)
+    all_users = SQLAlchemyConnectionField(User)
+    all_roles = SQLAlchemyConnectionField(Role)
+    role = relay.Node.Field(Role)
+
+
+class AddUser(relay.ClientIDMutation):
+    class Input:
+        url = graphene.String(required=True)
+        description = graphene.String()
+
+    image = graphene.Field(ImageNode)
+
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        pass
